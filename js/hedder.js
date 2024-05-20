e1f24c849d4de1f05bfbf08d884f7ca21a0ad60c
ww <ww@wws-MacBook-Air.local>
2024-05-20 15:11:11 +0300

    first commit

---
+++ js/hedder.js
@@ -0,0 +1,35 @@
+let valde = {
+name:'vlad',
+age: 20,
+img: null,
+hobbi: "basket",
+
+};
+console.log(valde);
+//!== не рівність 
+//=== рівність  
+//$$ логічне і .. || або .. ! ні  
+const vald = true;
+
+let obj ={}
+obj.name= 'omar';
+obj.age= 56;
+obj['hobby']='footbal' //можна різними способами писати обєкти 
+console.log(obj);
+
+const hey = 18 // напише по дефолту якщо не буде виконний жодний приклад 
+
+// switch  (hey){
+//     case 17:
+//         alert ('not have  18');
+//     break;
+//     case 19:
+//         alert ('good');
+//         break;
+//     default:
+//         alert ('not write')  ;  
+// }
+
+if (age === 19 && name === "vlad" && vald){
+  console.log(age);
+}
